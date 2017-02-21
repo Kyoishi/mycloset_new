1fa9ae32d6d9797db418e88b214092e4231c78f9
KYOHEI ISHIKAWA <kyoheiishikawa@KYOHEIs-MacBook-Air.local>
2017-02-21 09:18:53 +0900

    routesの変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :forms, only: :index
-  resources :outfits, only: :index, :new
-  resources :users, only: :index
+
+  resources :users, only: :index do
+    resources :outfits, only: [:index, :new]
 
   end
