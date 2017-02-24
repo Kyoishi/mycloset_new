76ee4effdc6d619b69c6494c44652d829b232757
KYOHEI ISHIKAWA <kyoheiishikawa@KYOHEIs-MacBook-Air.local>
2017-02-24 12:19:22 +0900

    heroku gemfile.lock削除前

--- app/models/outfit.rb
+++ app/models/outfit.rb
@@ -6,4 +6,6 @@ class Outfit < ActiveRecord::Base
     validates_attachment_content_type :image,
                                       content_type: ["image/jpg","image/jpeg","image/png"]
 
+  validates :image, presence: true
+
 end
