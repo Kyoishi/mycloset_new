8d0c129a83b959bdf26bf2defe97169ad56b6982
KYOHEI ISHIKAWA <kyoheiishikawa@KYOHEIs-MacBook-Air.local>
2017-02-27 17:29:24 +0900

    認証できず．．．

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170226112437) do
+ActiveRecord::Schema.define(version: 20170227074414) do
 
   create_table "coordinates", force: :cascade do |t|
     t.integer  "day_id",     limit: 4
@@ -62,6 +62,10 @@ ActiveRecord::Schema.define(version: 20170226112437) do
     t.integer  "avatar_file_size",       limit: 4
     t.datetime "avatar_updated_at"
     t.string   "nickname",               limit: 255
+    t.string   "confirmation_token",     limit: 255
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email",      limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
