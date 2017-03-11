6ebb18b9efa1f5ec25db2c47cc7f9d44e336b217
KYOHEI ISHIKAWA <kyoheiishikawa@KYOHEIs-MacBook-Air.local>
2017-03-11 20:40:08 +0900

    try4

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,11 +94,11 @@ Rails.application.configure do
 
   config.paperclip_defaults = {
   :storage => :s3,
-  :bucket => ENV['AMAZON_S3_BUCKET_NAME'],
-  :s3_region => ENV['AMAZON_S3_REGION'],
+  :bucket => ENV['S3_BUCKET_NAME'],
+  :s3_region => ENV['AWS_REGION'],
   :s3_credentials => {
-    :access_key_id => ENV['AMAZON_ACCESS_KEY_ID'],
-    :secret_access_key => ENV['AMAZON_SECRET_ACCESS_KEY']
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
   }
 }
 
