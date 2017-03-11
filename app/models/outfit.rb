647308e6d20273a62cc3c3e481d7e294f47b569e
KYOHEI ISHIKAWA <kyoheiishikawa@KYOHEIs-MacBook-Air.local>
2017-03-11 18:48:04 +0900

    host name削除

--- app/models/outfit.rb
+++ app/models/outfit.rb
@@ -4,7 +4,6 @@ class Outfit < ActiveRecord::Base
     has_many :days, :through => :coordinates,dependent: :destroy
 
     has_attached_file :image,
-                      :s3_host_name {'s3-ap-northeast-1.amazonaws.com'}
                       styles:  { medium: "300x300#", thumb: "100x100#" }
 
     validates_attachment_content_type :image,
