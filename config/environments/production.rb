4f4f2477edd4215dcf301486b6d35710f1a960b2
KYOHEI ISHIKAWA <kyoheiishikawa@KYOHEIs-MacBook-Air.local>
2017-03-11 19:59:27 +0900

    delete

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,7 +95,6 @@ Rails.application.configure do
   config.paperclip_defaults = {:storage        => :s3,
   :bucket         => ENV['S3_BUCKET_NAME'],
   :s3_region      => ENV['AWS_REGION'],
-  :s3_host_name   => 's3-ap-northeast-1.amazonaws.com',
   :s3_credentials => {
     access_key_id: ENV['AWS_ACCESS_KEY_ID'],
     secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
