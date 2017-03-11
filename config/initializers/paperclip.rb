d64c63661d0baf53c148c1f850374a1633dbdc7d
KYOHEI ISHIKAWA <kyoheiishikawa@KYOHEIs-MacBook-Air.local>
2017-03-11 22:47:38 +0900

    config change

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,4 +1,4 @@
 
-Paperclip::Attachment.default_options[:url] = 'upload-mycloset.us-east-1.amazonaws.com'
+Paperclip::Attachment.default_options[:url] = 'upload-mycloset.s3.amazonaws.com'
 Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
-Paperclip::Attachment.default_options[:s3_host_name] = 'us-east-1.amazonaws.com'
+Paperclip::Attachment.default_options[:s3_host_name] = 's3.amazonaws.com'
