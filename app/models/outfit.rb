fc69f2592d1b3c8f51a9199a718ad511a658d269
KYOHEI ISHIKAWA <kyoheiishikawa@KYOHEIs-MacBook-Air.local>
2017-03-11 18:24:55 +0900

    huuuuu

--- app/models/outfit.rb
+++ app/models/outfit.rb
@@ -9,7 +9,7 @@ class Outfit < ActiveRecord::Base
                       :access_key_id => S3KEY,
                       :secret_access_key => S3SECRET,
                       :s3_host_name =>  "s3-ap-northeast-1.amazonaws.com"
-                    },
+                    }
                       styles:  { medium: "300x300#", thumb: "100x100#" }
 
     validates_attachment_content_type :image,
