7c2f8e309cbe568136cb8b683f3cd69159317ddd
KYOHEI ISHIKAWA <kyoheiishikawa@KYOHEIs-MacBook-Air.local>
2017-03-11 18:21:50 +0900

    another one

--- app/models/outfit.rb
+++ app/models/outfit.rb
@@ -4,6 +4,12 @@ class Outfit < ActiveRecord::Base
     has_many :days, :through => :coordinates,dependent: :destroy
 
     has_attached_file :image,
+                    :storage => :s3,
+                    :s3_credentials => {
+                      :access_key_id => S3KEY,
+                      :secret_access_key => S3SECRET,
+                      :s3_host_name =>  "s3-ap-northeast-1.amazonaws.com"
+                    },
                       styles:  { medium: "300x300#", thumb: "100x100#" }
 
     validates_attachment_content_type :image,
