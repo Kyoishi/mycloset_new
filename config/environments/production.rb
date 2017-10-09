94a3fa9d4816c5a1b35ecd1f0fc802d72996476c
KYOHEI ISHIKAWA <kyoheiishikawa@KYOHEIs-MacBook-Air.local>
2017-10-09 21:26:08 +0900

    secret keybase

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,16 +78,17 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = { host: 'morning-refuge-86803.herokuapp.com'}
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :enable_starttls_auto => true,
     :address => "smtp.gmail.com",
     :port => 587,
-    :domain => 'smtp.gmail.com',
+    :domain => 'gmail.com',
     :user_name => "favclosetinfo@gmail.com", #ご自身のgmailアドレス
     :password => "Ladmusician335", #ご自身のgmailアドレスのパスワード
-    :authentication => 'login'
+    :authentication => 'plain'
+    :enable_starttls_auto => true
   }
 
   # s3の設定
