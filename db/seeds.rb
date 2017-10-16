01a696980273181df244db6d8e63b88b5adcc108
KYOHEI ISHIKAWA <kyoheiishikawa@KYOHEIs-MacBook-Air.local>
2017-10-16 21:49:28 +0900

    category

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,4 +11,10 @@ days_csv = CSV.readlines("db/days_view.csv")
 days_csv.shift
 days_csv.each do |row|
   Day.create(date: row[3])
+
+categories_csv = CSV.readlines("db/categories_view.csv")
+categories_csv.shift
+categories_csv.each do |row|
+  Category.create(name: row[1], parent_id: row[2], display: row[3], level: row[6] )
+
 end
