246757ec44a2ce5a29809f53cf2777950e7bbcda
KYOHEI ISHIKAWA <kyoheiishikawa@KYOHEIs-MacBook-Air.local>
2017-10-18 22:15:40 +0900

    categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,9 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 require "csv"
 
-days_csv = CSV.readlines("db/days.csv")
-days_csv.shift
-days_csv.each do |row|
-  Day.create(date: row[3])
+categories_csv = CSV.readlines("db/categories_view.csv")
+categories_csv.shift
+categories_csv.each do |row|
+  Day.create(name: row[1])
 
 end
