5483ec1e15671a0b3f7de493912cb8f6ffdafbd4
KYOHEI ISHIKAWA <kyoheiishikawa@KYOHEIs-MacBook-Air.local>
2017-10-24 22:13:45 +0900

    paperclip setting

--- app/models/outfit.rb
+++ app/models/outfit.rb
@@ -3,11 +3,7 @@ class Outfit < ActiveRecord::Base
     belongs_to :category
 
     has_attached_file :image,
-                      styles:  { medium: "300x300#", thumb: "100x100#" },
-    :storage => :s3,
-    :s3_permissions => :private,
-    :s3_credentials => "#{Rails.root}/config/s3.yml",
-    :path => ":class/:attachment/:id/:style.:extension"
+                      styles:  { medium: "300x300#", thumb: "100x100#" }
 
     validates_attachment_content_type :image,
                                       content_type: ["image/jpg","image/jpeg","image/png"]
