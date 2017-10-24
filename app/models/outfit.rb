5f1ee678be94686d93253fbccd142c9969672d15
KYOHEI ISHIKAWA <kyoheiishikawa@KYOHEIs-MacBook-Air.local>
2017-10-24 22:06:00 +0900

    outfit model

--- app/models/outfit.rb
+++ app/models/outfit.rb
@@ -3,7 +3,11 @@ class Outfit < ActiveRecord::Base
     belongs_to :category
 
     has_attached_file :image,
-                      styles:  { medium: "300x300#", thumb: "100x100#" }
+                      styles:  { medium: "300x300#", thumb: "100x100#" },
+    :storage => :s3,
+    :s3_permissions => :private,
+    :s3_credentials => "#{Rails.root}/config/s3.yml",
+    :path => ":class/:attachment/:id/:style.:extension"
 
     validates_attachment_content_type :image,
                                       content_type: ["image/jpg","image/jpeg","image/png"]
